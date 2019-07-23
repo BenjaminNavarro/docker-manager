1ada326608f8b2d798cdc9c5c8f5bff8666e8939
Benjamin Navarro <navarro.benjamin13@gmail.com>
2019-07-23 17:52:54 +0200

    Better handling of extra_flags

--- parser.go
+++ parser.go
@@ -97,8 +97,9 @@ func (configuration *ImageConfiguration) GenerateStartCommand() []string {
 		push("--cap-drop=" + configuration.Capabilities.Drop[idx])
 	}
 
-	if configuration.ExtraFlags != "" {
-		push(configuration.ExtraFlags)
+	tokens := strings.Split(configuration.ExtraFlags, " ")
+	for tokenIdx := range tokens {
+		push(tokens[tokenIdx])
 	}
 
 	push(configuration.GetImageWithTag())
